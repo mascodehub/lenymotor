8f4a5607224205e7bfbe269dd96c9e16b35496b3
wabot <walidshr8@gmail.com>
2025-10-24 22:04:13 +0700

    update: layouting mobile navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,20 @@
-$(document).on('click', '#btnNavbar', function(){
-   
+$(document).on('click', '#btnNavbar', function (e) {
+    e.stopPropagation();
+    $('#navbarmob, #overlay').toggleClass('show');
+});
+
+// Klik tombol "Tutup"
+$(document).on('click', '#btnCloseMenubar', function (e) {
+    e.stopPropagation();
+    $('#navbarmob, #overlay').removeClass('show');
+});
+
+// Klik di luar area navbar = tutup
+$(document).on('click', function (e) {
+    const nav = $('#navbarmob');
+    const overlay = $('#overlay');
+    if (nav.hasClass('show') && !nav.is(e.target) && nav.has(e.target).length === 0) {
+        nav.removeClass('show');
+        overlay.removeClass('show');
+    }
 });
