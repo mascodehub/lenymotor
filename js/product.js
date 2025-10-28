c598fceda93f2f1da00050489f7ed52dfec18e4c
walid <mohamad.walid@spil.co.id>
2025-10-28 16:03:27 +0700

    update: product filter sort mobile

--- js/product.js
+++ js/product.js
@@ -112,109 +112,119 @@ $(document).ready(async function () {
 
     renderFilter();
 
-    const sliderPriceMin = $('#slider-price-min');
-    const sliderPriceMax = $('#slider-price-max');
-    const rangePriceMin = $('#range-price-min');
-    const rangePriceMax = $('#range-price-max');
-    const trackPrice = $('#slider-price-track');
-    const minPriceGap = parseInt(sliderPriceMin.attr('step'));
-
     function formatRupiah(num) {
         return 'Rp ' + num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
 
-    function fillTrackPrice() {
-        const minVal = parseInt(sliderPriceMin.val(), 10);
-        const maxVal = parseInt(sliderPriceMax.val(), 10);
-        const minAllowed = parseInt(sliderPriceMin.attr('min'), 10);
-        const maxAllowed = parseInt(sliderPriceMin.attr('max'), 10);
-        const range = maxAllowed - minAllowed || 1;
+    function renderSliderPrice(form_id){
+        const sliderPriceMin = $(`${form_id} #slider-price-min`);
+        const sliderPriceMax = $(`${form_id} #slider-price-max`);
+        const rangePriceMin = $(`${form_id} #range-price-min`);
+        const rangePriceMax = $(`${form_id} #range-price-max`);
+        const trackPrice = $(`${form_id} #slider-price-track`);
+        const minPriceGap = parseInt(sliderPriceMin.attr('step'));
 
-        let percent1 = ((minVal - minAllowed) / range) * 100;
-        let percent2 = ((maxVal - minAllowed) / range) * 100;
+        function fillTrackPrice() {
+            const minVal = parseInt(sliderPriceMin.val(), 10);
+            const maxVal = parseInt(sliderPriceMax.val(), 10);
+            const minAllowed = parseInt(sliderPriceMin.attr('min'), 10);
+            const maxAllowed = parseInt(sliderPriceMin.attr('max'), 10);
+            const range = maxAllowed - minAllowed || 1;
 
-        percent1 = Math.max(0, Math.min(100, percent1));
-        percent2 = Math.max(0, Math.min(100, percent2));
+            let percent1 = ((minVal - minAllowed) / range) * 100;
+            let percent2 = ((maxVal - minAllowed) / range) * 100;
 
-        trackPrice.css('background',
-            `linear-gradient(to right, #ccc ${percent1}%, #d40000 ${percent1}%, #d40000 ${percent2}%, #ccc ${percent2}%)`
-        );
-    }
+            percent1 = Math.max(0, Math.min(100, percent1));
+            percent2 = Math.max(0, Math.min(100, percent2));
 
-    function updatePriceValues() {
-        let minVal = parseInt(sliderPriceMin.val());
-        let maxVal = parseInt(sliderPriceMax.val());
-
-        if (maxVal - minVal <= minPriceGap) {
-            if (this.id === 'slider-price-min') {
-                sliderPriceMin.val(maxVal - minPriceGap);
-                minVal = maxVal - minPriceGap;
-            } else {
-                sliderPriceMax.val(minVal + minPriceGap);
-                maxVal = minVal + minPriceGap;
-            }
+            trackPrice.css('background',
+                `linear-gradient(to right, #ccc ${percent1}%, #d40000 ${percent1}%, #d40000 ${percent2}%, #ccc ${percent2}%)`
+            );
         }
 
-        rangePriceMin.text(formatRupiah(minVal));
-        rangePriceMax.text(formatRupiah(maxVal));
-        fillTrackPrice();
-    }
+        function updatePriceValues() {
+            let minVal = parseInt(sliderPriceMin.val());
+            let maxVal = parseInt(sliderPriceMax.val());
 
-    updatePriceValues.call(sliderPriceMin[0]);
-    updatePriceValues.call(sliderPriceMax[0]);
-
-    sliderPriceMin.on('input', updatePriceValues);
-    sliderPriceMax.on('input', updatePriceValues);
-
-    const sliderYearMin = $('#slider-year-min');
-    const sliderYearMax = $('#slider-year-max');
-    const rangeYearMin = $('#range-year-min');
-    const rangeYearMax = $('#range-year-max');
-    const trackYear = $('#slider-year-track');
-    const minYearGap = parseInt(sliderYearMin.attr('step'));
-
-    function fillTrackYear() {
-        const minVal = parseInt(sliderYearMin.val(), 10);
-        const maxVal = parseInt(sliderYearMax.val(), 10);
-        const minAllowed = parseInt(sliderYearMin.attr('min'), 10);
-        const maxAllowed = parseInt(sliderYearMin.attr('max'), 10);
-        const range = maxAllowed - minAllowed || 1;
-
-        let percent1 = ((minVal - minAllowed) / range) * 100;
-        let percent2 = ((maxVal - minAllowed) / range) * 100;
-
-        percent1 = Math.max(0, Math.min(100, percent1));
-        percent2 = Math.max(0, Math.min(100, percent2));
-
-        trackYear.css('background',
-            `linear-gradient(to right, #ccc ${percent1}%, #d40000 ${percent1}%, #d40000 ${percent2}%, #ccc ${percent2}%)`
-        );
-    }
-
-    function updateYearValues() {
-        let minVal = parseInt(sliderYearMin.val());
-        let maxVal = parseInt(sliderYearMax.val());
-
-        if (maxVal - minVal <= minYearGap) {
-            if (this.id === 'slider-year-min') {
-                sliderYearMin.val(maxVal - minYearGap);
-                minVal = maxVal - minYearGap;
-            } else {
-                sliderYearMax.val(minVal + minYearGap);
-                maxVal = minVal + minYearGap;
+            if (maxVal - minVal <= minPriceGap) {
+                if (this.id === 'slider-price-min') {
+                    sliderPriceMin.val(maxVal - minPriceGap);
+                    minVal = maxVal - minPriceGap;
+                } else {
+                    sliderPriceMax.val(minVal + minPriceGap);
+                    maxVal = minVal + minPriceGap;
+                }
             }
+
+            rangePriceMin.text(formatRupiah(minVal));
+            rangePriceMax.text(formatRupiah(maxVal));
+            fillTrackPrice();
         }
 
-        rangeYearMin.text((minVal));
-        rangeYearMax.text((maxVal));
-        fillTrackYear();
+        updatePriceValues.call(sliderPriceMin[0]);
+        updatePriceValues.call(sliderPriceMax[0]);
+
+        sliderPriceMin.on('input', updatePriceValues);
+        sliderPriceMax.on('input', updatePriceValues);
     }
 
-    updateYearValues.call(sliderYearMin[0]);
-    updateYearValues.call(sliderYearMax[0]);
+    renderSliderPrice('#form-filter-price');
+    renderSliderPrice('#form-filter-price-mob');
 
-    sliderYearMin.on('input', updateYearValues);
-    sliderYearMax.on('input', updateYearValues);
+    function renderSliderYear(form_id){
+        const sliderYearMin = $(`${form_id} #slider-year-min`);
+        const sliderYearMax = $(`${form_id} #slider-year-max`);
+        const rangeYearMin = $(`${form_id} #range-year-min`);
+        const rangeYearMax = $(`${form_id} #range-year-max`);
+        const trackYear = $(`${form_id} #slider-year-track`);
+        const minYearGap = parseInt(sliderYearMin.attr('step'));
+
+        function fillTrackYear() {
+            const minVal = parseInt(sliderYearMin.val(), 10);
+            const maxVal = parseInt(sliderYearMax.val(), 10);
+            const minAllowed = parseInt(sliderYearMin.attr('min'), 10);
+            const maxAllowed = parseInt(sliderYearMin.attr('max'), 10);
+            const range = maxAllowed - minAllowed || 1;
+
+            let percent1 = ((minVal - minAllowed) / range) * 100;
+            let percent2 = ((maxVal - minAllowed) / range) * 100;
+
+            percent1 = Math.max(0, Math.min(100, percent1));
+            percent2 = Math.max(0, Math.min(100, percent2));
+
+            trackYear.css('background',
+                `linear-gradient(to right, #ccc ${percent1}%, #d40000 ${percent1}%, #d40000 ${percent2}%, #ccc ${percent2}%)`
+            );
+        }
+
+        function updateYearValues() {
+            let minVal = parseInt(sliderYearMin.val());
+            let maxVal = parseInt(sliderYearMax.val());
+
+            if (maxVal - minVal <= minYearGap) {
+                if (this.id === 'slider-year-min') {
+                    sliderYearMin.val(maxVal - minYearGap);
+                    minVal = maxVal - minYearGap;
+                } else {
+                    sliderYearMax.val(minVal + minYearGap);
+                    maxVal = minVal + minYearGap;
+                }
+            }
+
+            rangeYearMin.text((minVal));
+            rangeYearMax.text((maxVal));
+            fillTrackYear();
+        }
+
+        updateYearValues.call(sliderYearMin[0]);
+        updateYearValues.call(sliderYearMax[0]);
+
+        sliderYearMin.on('input', updateYearValues);
+        sliderYearMax.on('input', updateYearValues);
+    }
+    
+    renderSliderYear('#form-filter-year');
+    renderSliderYear('#form-filter-year-mob');
 
     $('#spanFilterProduct').text(`Menampilkan ${PRODUCT_IMAGES.length} Motor`);
 
@@ -357,6 +367,32 @@ $(document).ready(async function () {
         if (currentPage < totalPages) {
             currentPage++;
             renderPagination();
+        }
+    });
+
+    $("#btn-filter-mob").click(function(e){
+        e.stopPropagation();
+        $('body').toggleClass('no-scroll'); // ketika form muncul
+        $('#form-filter-mob, #overlay-product').toggleClass('show');
+    })
+
+    // $("#btn-filter-mob").click()
+
+    $("#btn-sort-mob").click(function(e){
+        e.stopPropagation();
+        $('body').toggleClass('no-scroll'); // ketika form muncul
+        $('#form-sort-mob, #overlay-product').toggleClass('show');
+    })
+
+    $(document).on('click', function (e) {
+
+        if ($(e.target).is('input[type="checkbox"], label')) return;
+        const nav = $('#form-filter-mob, #form-sort-mob');
+        const overlay = $('#overlay-product');
+        if (nav.hasClass('show') && !nav.is(e.target) && nav.has(e.target).length === 0) {
+            nav.removeClass('show');
+            overlay.removeClass('show');
+            $('body').removeClass('no-scroll'); // ketika form ditutup
         }
     });
 })
